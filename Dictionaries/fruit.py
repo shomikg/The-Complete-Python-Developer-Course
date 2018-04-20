c9ef8cf61ad015fa397e1b45893e47859b5e7d51
Shomik Goswami <shomikprogrammer@gmail.com>
2018-04-20 19:46:26 +0530

    creating for mroe dictionaries

---
+++ Dictionaries/fruit.py
@@ -0,0 +1,16 @@
+fruit = {"orange": "a sweet, orange, citrus fruit",
+         "apple": "good for making cider",
+         "lemon": "a sour, yellow citrus fruit",
+         "grape": "a small, sweet fruit growing in bunches",
+         "lime": "a sour, green citrus fruit"}
+
+print(fruit)
+
+veg = {"cabbage": "every child's favourite",
+       "sprouts": "mmmmm, lovely",
+       "spinach": "can I have some more fruit, please"}
+
+print(veg)
+
+veg.update(fruit)
+print(veg)
