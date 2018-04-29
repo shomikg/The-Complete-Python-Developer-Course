4119d5e8fecd6b03d1260213280b79c06d44f18a
Shomik Goswami <shomikprogrammer@gmail.com>
2018-04-29 22:05:35 +0530

    After Sets 2

--- Sets/sets.py
+++ Sets/sets.py
@@ -46,6 +46,27 @@
 # print(squares.intersection(even))
 # print(squares & even)
 
+# even = set(range(0, 40, 2))
+# print(sorted(even))
+# squares_tuple = (4, 6, 9, 16, 25)
+#
+# squares = set(squares_tuple)
+# print(sorted(squares))
+#
+# print("even minus squares")
+# print(sorted(even.difference(squares)))
+# print(sorted(even - squares))
+#
+# print("squares minus even")
+# print(squares.difference(even))
+# print(squares - even)
+#
+# print("=" * 40)
+# print(sorted(even))
+# print(squares)
+# even.difference_update(squares)
+# print(sorted(even))
+
 even = set(range(0, 40, 2))
 print(sorted(even))
 squares_tuple = (4, 6, 9, 16, 25)
@@ -53,17 +74,34 @@ squares_tuple = (4, 6, 9, 16, 25)
 squares = set(squares_tuple)
 print(sorted(squares))
 
-print("even minus squares")
-print(sorted(even.difference(squares)))
-print(sorted(even - squares))
+# print("symmetric even minus squares")
+# print(sorted(even.symmetric_difference(squares)))
+#
+# print("symmetric squares minus even")
+# print(sorted(squares.symmetric_difference(even)))
 
-print("squares minus even")
-print(squares.difference(even))
-print(squares - even)
+# squares.discard(4)
+# squares.remove(16)
+# squares.discard(8)
+# print(squares)
+#
+# try:
+#     squares.remove(8)
+# except KeyError:
+#     print("The item 8 is not a member of the set")
 
+even = set(range(0, 40, 2))
+print(even)
+squares_tuple = (4, 6, 16)
 
+squares = set(squares_tuple)
+print(squares)
 
+if squares.issubset(even):
+    print("squares is a subset of even")
 
+if even.issuperset(squares):
+    print("even is a superset of squares")
 
 
 
